b79cafca296a828b57f3eacc526cbc7f920c4909
= <jeremiah.simonsen@maine.edu>
2015-03-04 21:57:44 -0500

    Added babysteps main

---
+++ xbee/babysteps/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <termios.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+int main(int argc, char *argv[])
+{
+	//TODO write config serial port function
+
+	//TODO write config xbee function
+
+	//TODO read and write some stuff between XBee's
+
+	return 0;
+}
