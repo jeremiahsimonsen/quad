c2b5b7536243561dc393496cc22a4e5a77763d42
Jeremiah Simonsen <jeremiah.simonsen@maine.edu>
2015-09-19 20:04:49 -0400

    Initial commit

---
+++ ADC/src/main.cpp
@@ -0,0 +1,51 @@
+//
+// This file is part of the GNU ARM Eclipse distribution.
+// Copyright (c) 2014 Liviu Ionescu.
+//
+
+// ----------------------------------------------------------------------------
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "diag/Trace.h"
+#include "Adc.h"
+
+// ----------------------------------------------------------------------------
+//
+// Standalone STM32F4 empty sample (trace via DEBUG).
+//
+// Trace support is enabled by adding the TRACE macro definition.
+// By default the trace messages are forwarded to the DEBUG output,
+// but can be rerouted to any device or completely suppressed, by
+// changing the definitions required in system/src/diag/trace_impl.c
+// (currently OS_USE_TRACE_ITM, OS_USE_TRACE_SEMIHOSTING_DEBUG/_STDOUT).
+//
+
+// ----- main() ---------------------------------------------------------------
+
+// Sample pragmas to cope with warnings. Please note the related line at
+// the end of this function, used to pop the compiler diagnostics status.
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#pragma GCC diagnostic ignored "-Wmissing-declarations"
+#pragma GCC diagnostic ignored "-Wreturn-type"
+
+int
+main(int argc, char* argv[])
+{
+	// At this stage the system clock should have already been configured
+	// at high speed.
+
+	Adc adc(AdcPin::PA2);
+
+	// Infinite loop
+	while (1)
+	{
+		uint32_t adcVal = adc.read();
+		trace_printf("%d\n",adcVal);
+	}
+}
+
+#pragma GCC diagnostic pop
+
+// ----------------------------------------------------------------------------
