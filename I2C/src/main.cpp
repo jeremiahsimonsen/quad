bcd4ca70462ddcfbe462efc941a2c2b7eb415847
Jeremiah Simonsen <jeremiah.simonsen@maine.edu>
2015-10-05 21:29:48 -0400

    Switched to using class

--- I2C/src/main.cpp
+++ I2C/src/main.cpp
@@ -33,18 +33,18 @@ int main(int argc, char* argv[])
 //	HAL_Init();
 
 	uint8_t slave_addr = 0b10111010;
-//	I2C i2c(i2cPin::PB6, i2cPin::PB9);
-	initI2C((int)i2cPin::PB6, (int)i2cPin::PB9);
+	I2C i2c(i2cPin::PB6, i2cPin::PB9);
+//	initI2C((int)i2cPin::PB6, (int)i2cPin::PB9);
 
 	uint8_t data[3] = {0x0};
 
 	uint8_t on = 0xB4;
 	// Power on and set output data rate to 12.5 Hz
 //	i2c.memWrite(slave_addr, 0x20, &on, 1);
-//	I2C_MemWrite(slave_addr, 0x20, &on, 1);
-	while(HAL_I2C_GetState(&i2cHandle) != HAL_I2C_STATE_READY);
-	if (HAL_I2C_Mem_Write(&i2cHandle, slave_addr, 0x20, I2C_MEMADD_SIZE_8BIT, &on, 1, 1000) != HAL_OK)
-		while(1);
+	I2C_MemWrite(slave_addr, 0x20, &on, 1);
+//	while(HAL_I2C_GetState(&i2cHandle) != HAL_I2C_STATE_READY);
+//	if (HAL_I2C_Mem_Write(&i2cHandle, slave_addr, 0x20, I2C_MEMADD_SIZE_8BIT, &on, 1, 1000) != HAL_OK)
+//		while(1);
 
 	// Infinite loop
 	while (1)
@@ -53,13 +53,13 @@ int main(int argc, char* argv[])
 		uint8_t x;
 
 //		i2c.memRead(slave_addr, 0x28|(1<<7), data, 3);
-//		i2c.memRead(slave_addr, 0x2B|(1<<7), (uint8_t*)data, 2);
+		i2c.memRead(slave_addr, 0x2B|(1<<7), data, 2);
 //		i2c.memRead(slave_addr, 0x0F, &x, 1);
 //		I2C_Mem_Read(slave_addr, 0x2B|(1<<7), (uint8_t *)data, 2);
-		while(HAL_I2C_GetState(&i2cHandle) != HAL_I2C_STATE_READY);
-		if (HAL_I2C_Mem_Read(&i2cHandle, slave_addr, 0x2B|(1<<7), I2C_MEMADD_SIZE_8BIT, data, 2, 1000) != HAL_OK) {
-			while(1);
-		}
+//		while(HAL_I2C_GetState(&i2cHandle) != HAL_I2C_STATE_READY);
+//		if (HAL_I2C_Mem_Read(&i2cHandle, slave_addr, 0x2B|(1<<7), I2C_MEMADD_SIZE_8BIT, data, 2, 1000) != HAL_OK) {
+//			while(1);
+//		}
 
 //		trace_printf("Temp_L = %d\n", data[0]);
 //		trace_printf("Temp_H = %d\n", data[1]);
