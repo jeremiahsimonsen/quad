d88adfb5800eb71d7485a1c503de3f0a9941e067
Jeremiah Simonsen <jeremiah.simonsen@maine.edu>
2015-10-05 23:17:31 -0400

    Started LPS25H class and added license

---
+++ I2C/include/LPS25H.h
@@ -0,0 +1,75 @@
+/**
+ * @file LPS25H.h
+ *
+ * @brief Class for interfacing with the LPS25H pressure sensor
+ *
+ * @author Jasmine Despres
+ * @author Jeremiah Simonsen
+ *
+ * @date Oct 5, 2015
+ *
+ * Based on lps-arduino library from Pololu Corp. See license.txt for details
+ *
+ */
+
+#ifndef I2C_H_
+#define I2C_H_
+
+enum class LPS25H_Reg {
+	REF_P_XL 		=	0x08,
+	REF_P_L			=	0x09,
+	REF_P_H			= 	0x0A,
+
+	WHO_AM_I 		= 	0x0F,
+
+	RES_CONF		=	0x10,
+
+	CTRL_REG1		=	0x20,
+	CTRL_REG2		=	0x21,
+	CTRL_REG3		=	0x22,
+	CTRL_REG4		=	0x23,
+
+	INT_CFG			=	0x24,
+	INT_SOURCE		=	0x25,
+
+	STATUS_REG		=	0x27,
+
+	PRESS_OUT_XL	=	0x28,
+	PRESS_OUT_L		=	0x29,
+	PRESS_OUT_H		= 	0x2A,
+
+	TEMP_OUT_L		=	0x2B,
+	TEMP_OUT_H		=	0x2C,
+
+	FIFO_CTRL		=	0x2E,
+	FIFO_STATUS		=	0x2F,
+
+	THS_P_L			= 	0x30,
+	THS_P_H			= 	0x31,
+
+	RPDS_L			=	0x39,
+	RPDS_H			=	0x3A
+};
+
+class LPS25H {
+private:
+	I2C *i2c;
+	uint8_t address;
+
+public:
+	LPS25H(void);
+
+	void enable(void);
+
+	float readPressureInchesHg(void);
+	int32_t readPressureRaw(void);
+
+	float readTemperatureF(void);
+	float reatTemperatureRaw(void);
+
+	float pressureToAltitudeFeet(float pressure_inHg, float altimeter_setting_inHg = 29.9213);
+};
+
+#endif
+
+
---
+++ I2C/src/LPS25H.cpp
@@ -0,0 +1,53 @@
+/**
+ * @file LPS25H.cpp
+ *
+ * @brief Class for interfacing with the LPS25H pressure sensor
+ *
+ * @author Jasmine Despres
+ * @author Jeremiah Simonsen
+ *
+ * @date Oct 5, 2015
+ *
+ * Based on lps-arduino library from Pololu Corp. See license.txt for details
+ *
+ */
+
+#include "LPS25H.h"
+#include "I2C.h"
+
+LPS25H::LPS25H(void) {
+	address = 0b10111010;
+	// TODO: Init i2c somehow...
+	enable();
+}
+
+void LPS25H::enable(void) {
+	uint8_t buff = 0xB4;
+
+	i2c->memWrite(address, (uint8_t)LPS25H_Reg::CTRL_REG1, &buff, 1);
+}
+
+float LPS25H::readPressureInchesHg(void) {
+
+}
+
+int32_t LPS25H::readPressureRaw(void) {
+
+}
+
+float LPS25H::readTemperatureF(void) {
+
+}
+
+float LPS25H::reatTemperatureRaw(void) {
+
+}
+
+float LPS25H::pressureToAltitudeFeet(float pressure_inHg, float altimeter_setting_inHg = 29.9213) {
+
+}
+
+
+
+
+
